a86e76099f80a704e42e41da0880752d104b9188
sivachinni1 <167642188+sivachinni1@users.noreply.github.com>
2024-12-29 17:55:55 +0530

    Create if_else.sh

---
+++ if_else.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# START #
+
+echo -e "Enter any value: \c"
+read -r a
+echo -e "Enter any value: \c"
+read -r b
+
+if [ $a -gt $b ]; then
+echo "$a is greater than $b"
+else
+echo "$b is greater than $a"
+fi
+# END #
